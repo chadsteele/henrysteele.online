b33a1f072c7227ab4a6bec828632c84f747be38a
chadsteele <chad@chadsteele.com>
2026-09-08 13:44:38 +0800

    rock'n roll

--- src/App.svelte
+++ src/App.svelte
@@ -12,16 +12,19 @@
   <header class="intro">
     <p class="eyebrow">My gap year and bucket list adventures</p>
     <h1>Hi!  I'm Henry</h1>
-    <div class="intro-copy">
-      <p>I'm looking for my next gig and volunteer opportunity. I'm spending this year traveling around the world, making a difference with animals, nature, and communities, and learning new skills as I go.</p>
-      <p>I graduated high school last spring with a 3.8 GPA.  I speak English and Chinese fluently and a little french, spanish and japanese.  I love mountains and play soccer, basketball, MMA and ultimate frisbee.  I love to travel, learn, and explore new things. I have a passion for animals, music, nature, and the environment. I also love to cook and play my guitar. I'm looking for opportunities to learn and grow as a person and make a positive impact on the world.</p>
-      <p>Please share this site with anyone you think might be interested in hiring me or offering me a volunteer opportunity. I'm open to anything and everything. I want to learn and grow as a person and make a positive impact on the world. Thank you for your time and consideration.</p>
-    </div>
-    <p>
+     <p>I speak: 🇺🇸 English and 🇨🇳 Chinese fluently, 🇫🇷 French (3 years), and a little 🇪🇸 Spanish, 🇯🇵 Japanese, and 🎸 Rock'n Roll</p>
+       <p>
     Let's talk!  
         ✉️  <a href="mailto:henrysogod@gmail.com">Email</a> &nbsp;
         ☎️  <a href="https://wa.me/+23057464071">WhatsApp</a>
     </p>
+    <div class="intro-copy">
+      <p>I'm looking for my next gig and volunteer opportunity. I'm spending this year traveling around the world, making a difference with animals, nature, and communities, and learning new skills as I go.</p>
+      <p>I graduated high school last spring with a 3.8 GPA.  I love mountains and play soccer, basketball, MMA and ultimate frisbee.  I love to travel, learn, and explore new things. I have a passion for animals, music, nature, and the environment. I also love to cook and play my guitar. I'm looking for opportunities to learn and grow as a person and make a positive impact on the world.</p>
+      <p>Please share this site with anyone you think might be interested in hiring me or offering me a volunteer opportunity. I'm open to anything and everything. I want to learn and grow as a person and make a positive impact on the world. Thank you for your time and consideration.</p>
+     
+    </div>
+  
     
   </header>
   <section class="gallery-grid" aria-label="Adventures">
@@ -29,4 +32,9 @@
       <GalleryCard {adventure} {index} />
     {/each}
   </section>
+   <p>
+    Let's talk!  
+        ✉️  <a href="mailto:henrysogod@gmail.com">Email</a> &nbsp;
+        ☎️  <a href="https://wa.me/+23057464071">WhatsApp</a>
+    </p>
 </main>
